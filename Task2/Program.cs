bb94f36b6101760b25c6a2802e8aaa0b63679a48
sofabonadyseva3@gmail.com <Sofya Bonadyseva>
2024-04-13 09:41:37 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"A(m, n) = {FunctionAkk(m, n)}");
+    }
+
+    static int FunctionAkk(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0 && m > 0)
+            return FunctionAkk(m - 1, 1);
+        else
+            return FunctionAkk(m - 1, FunctionAkk(m, n - 1));
+    }
+}
+
+
+
+
