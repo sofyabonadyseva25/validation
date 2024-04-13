b75898ecd3af666bcd74e3a01200f607bfdd298e
sofabonadyseva3@gmail.com <Sofya Bonadyseva>
2024-04-13 08:09:25 +0300

    Task 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+void F (int m,int n)
+{
+    if (m !=  n)
+    {
+        if(m > n)
+        {
+            F(m - 1, n);
+            System.Console.Write(m + " ");
+        }
+        else
+        {
+            F(m + 1, n);
+            System.Console.Write(m + " ");
+        }
+    }
+    else
+    {
+        Console.Write(m + " ");
+
+    }
+   
+}
+F(m, n);
