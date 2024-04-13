5952d58d33270a12d57671a524d5fbcc057066f5
sofabonadyseva3@gmail.com <Sofya Bonadyseva>
2024-04-13 10:40:48 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 2, 67, 89, 56, 3, 5 };
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            PrintArrayReverse(array, index - 1);
+        }
+    }
+}
+
